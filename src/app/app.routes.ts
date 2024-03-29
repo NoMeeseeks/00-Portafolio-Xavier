de968c95b2ff6809cd22b7027798d9fcf03a3da9
NoMeeseeks <Xavier.e.santamaria.ramirez@gmail.com>
2024-03-28 22:49:05 -0500

    Iniciando pagina para portafolio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Route } from '@angular/router';
+import { LayoutComponent } from 'app/layout/layout.component';
+
+// import { initialDataResolver } from 'app/app.resolvers';
+// import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+// import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+// import { LandingHomeComponent } from './modules/landing/home/home.component';
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        loadChildren: () => import('app/modules/landing/home/home.routes')
+    },
+];
